fdd05d8c89b26ac7d872afc845c6dffa0a95cdc3
莫锹文 <183795044@qq.com>
2023-09-03 21:11:50 +0800

    移除不需要的

--- Android/TUIKit/TIMCommon/timcommon/src/main/java/com/tencent/qcloud/tuikit/timcommon/interfaces/ICommonMessageAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.tencent.qcloud.tuikit.timcommon.interfaces;
-
-import com.tencent.qcloud.tuikit.timcommon.bean.TUIMessageBean;
-
-public interface ICommonMessageAdapter {
-    TUIMessageBean getItem(int position);
-
-    void notifyItemChanged(int position);
-}
